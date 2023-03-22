24f37b6f2e28edb187800dfb10b811fc052e0451
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-03-22 13:20:21 +0700

    Update Matrix.java
    Adding sum() function for summarize of two matrices
    Adding sub() function for subtract of two matrices
    Add new sort() and display() function with a parameter (int[ ][ ]), and etc.

--- lib/matrix/Matrix.java
+++ lib/matrix/Matrix.java
@@ -1,3 +1,7 @@
+// :: ------------------ :: //
+/* --   MATRIX BUILDER   -- */
+// :: ------------------ :: //
+
 package lib.matrix;
 
 // -**- Built-in Package -**- //
@@ -8,6 +12,7 @@ import java.lang.Math;
 // -**- Local Package -**- //
 import lib.matrix.NullMatrixException;
 import lib.matrix.MatrixArrayFullException;
+import lib.matrix.IllegalMatrixSizeException;
 
 // ** MATRIX CLASS ** //
 public class Matrix
@@ -63,7 +68,7 @@ public class Matrix
 
     // Fill column with list of Integer(s)
     public void add(int... values)
-    throws NullMatrixException, MatrixArrayFullException {
+    throws NullMatrixException, MatrixArrayFullException, IllegalMatrixSizeException {
         // Throw "NullMatrixException" when matrix array is null
         if (this.MATRIX == null) {
             throw new NullMatrixException();
@@ -77,11 +82,13 @@ public class Matrix
         else if (this.index >= this.ROWS) {
             throw new MatrixArrayFullException();
         }
-
-        // Length of values list shouldn't greater than matrix columns
-        if (values.length > this.COLS) {
-            System.out.println("[!] Error: Too much arguments; Max column: " + this.COLS);
-            System.exit(1);
+        // Length of values list shouldn't greater than total matrix columns
+        else if (values.length > this.COLS) {
+            throw new IllegalMatrixSizeException("Too much arguments; Max column: " + this.COLS);
+        }
+        // And shouldn't be less than total matrix columns
+        else if (values.length < this.COLS) {
+            throw new IllegalMatrixSizeException("Not enough argument");
         }
 
 
@@ -137,15 +144,124 @@ public class Matrix
 
 
 
+    //// ----------------------- ////
+    // -**- MATRIX OPERATIONS -**- //
+    //// ----------------------- ////
+
+    // -- Summation (Matrix object) -- //
+    public int[ ][ ] sum(Matrix other)
+    throws IllegalMatrixSizeException, NullMatrixException {
+        // Throw "NullMatrixException" if matrix is null
+        if (this.MATRIX == null || other.MATRIX == null) {
+            throw new NullMatrixException();
+        }
+        // Else throw "IllegalMatrixSizeException" if the matrices are not same size
+        else if (this.ROWS != other.ROWS || this.COLS != other.COLS) {
+            throw new IllegalMatrixSizeException();
+        }
+
+        // Create new matrix for the result
+        int[ ][ ] result = new int[this.ROWS][other.COLS];
+
+        // Iterate over each element of the matrices and add the corresponding values together
+        for (int i = 0; i < this.ROWS; i++) {
+            for (int j = 0; j < this.COLS; j++) {
+                result[i][j] = this.MATRIX[i][j] + other.MATRIX[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    // -- Summation (Integer array) -- //
+    public int[ ][ ] sum(int[ ][ ] matrix)
+    throws IllegalMatrixSizeException, NullMatrixException {
+        // Throw "NullMatrixException" if matrix is null
+        if (this.MATRIX == null || matrix == null) {
+            throw new NullMatrixException();
+        }
+        // Else throw "IllegalMatrixSizeException" if the matrices are not same size
+        else if (this.ROWS != matrix.length || this.COLS != matrix[0].length) {
+            throw new IllegalMatrixSizeException();
+        }
+
+        // Create new matrix for the result
+        int[ ][ ] result = new int[this.ROWS][matrix[0].length];
+
+        // Using nested loop for iterate over each element of matrix
+        for (int i = 0; i < this.ROWS; i++) {
+            for (int j = 0; j < this.COLS; j++) {
+                result[i][j] = this.MATRIX[i][j] + matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+
+    // -- Subtraction (Matrix object) -- //
+    public int[ ][ ] sub(Matrix other)
+    throws IllegalMatrixSizeException, NullMatrixException {
+        // Throw "NullMatrixException" if matrix is null
+        if (this.MATRIX == null || other.MATRIX == null) {
+            throw new NullMatrixException();
+        }
+        // Else throw "IllegalMatrixSizeException" if the matrices are not same size
+        else if (this.ROWS != other.ROWS || this.COLS != other.COLS) {
+            throw new IllegalMatrixSizeException();
+        }
+
+        // Create new matrix for the result
+        int[ ][ ] result = new int[this.ROWS][other.COLS];
+
+        // Iterate over each element of all matrices and subtract each values together
+        for (int i = 0; i < this.ROWS; i++) {
+            for (int j = 0; j < this.COLS; j++) {
+                result[i][j] = this.MATRIX[i][j] - other.MATRIX[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    // -- Subtraction (Integer array) -- //
+    public int[ ][ ] sub(int[ ][ ] matrix)
+    throws IllegalMatrixSizeException, NullMatrixException {
+        // Throw "NullMatrixException" if matrix is null
+        if (this.MATRIX == null || matrix == null) {
+            throw new NullMatrixException();
+        }
+        // Else throw "IllegalMatrixSizeException" if the matrices are not same size
+        else if (this.ROWS != matrix.length || this.COLS != matrix[0].length) {
+            throw new IllegalMatrixSizeException();
+        }
+
+        // Create new matrix for the result
+        int[ ][ ] result = new int[this.ROWS][matrix[0].length];
+
+        // Iterate over each element of all matrices and subtract each values together
+        for (int i = 0; i < this.ROWS; i++) {
+            for (int j = 0; j < this.COLS; j++) {
+                result[i][j] = this.MATRIX[i][j] - matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+    //// ----------------------------- ////
+    // -**- [END] MATRIX OPERATIONS -**- //
+    //// ----------------------------- ////
+
+
     // -- Matrix Displayer -- //
     /** ---
     - Output example:
           [   [ n, n, n, ... ]
               [ n, n, n, ... ]
-              [ n, n, n, ... ]   ]
+              [ .. .. .. ... ]   ]
     --- **/
     public final void display() throws NullMatrixException {
-        // Throw "NullMatrixException" when matrix array is null
+        // Throw "NullMatrixException" if matrix array is null
         if (this.MATRIX == null) {
             throw new NullMatrixException();
         }
@@ -165,6 +281,32 @@ public class Matrix
         System.out.println("   ]");
     }
 
+    public static final void display(int[ ][ ] matrix) throws NullMatrixException {
+        // Throw "NullMatrixException" if matrix array is null
+        if (matrix == null) {
+            throw new NullMatrixException();
+        }
+
+        final int rows = matrix.length;
+        final int cols = matrix[0].length;
+
+        System.out.print("[   ");
+        for (int r = 0; r < rows; r++) {
+            System.out.print("[ ");
+            for (int c = 0; c < cols; c++) {
+                System.out.print(matrix[r][c]);
+                if (c != cols - 1) System.out.print(", ");
+            }
+            System.out.print(" ]");
+            if (r != rows - 1) {
+                System.out.print("\n    ");
+            }
+        }
+        System.out.println("   ]");
+    }
+
+
+    // -- Matrix Cleaner (assign 0 to each element) -- //
     public void clear() {
         for (int r = 0; r < this.ROWS; r++) {
             for (int c = 0; c < this.COLS; c++) {
@@ -178,6 +320,12 @@ public class Matrix
         // Just simply iterate matrix rows, then sort each columns
         for (int r = 0; r < this.ROWS; r++) {
             Arrays.sort(this.MATRIX[r]);
+        }
+    }
+
+    public static void sort(int[ ][ ] matrix) {
+        for (int r = 0; r < matrix.length; r++) {
+            Arrays.sort(matrix[r]);
         }
     }
 
