c7076278e6759c4cb18551d62c674e9ac389efba
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-04-18 20:03:57 +0700

    Update `OSUtils.java`
    - Added javadoc to all public methods and variables.

--- src/com/mitsuki/jmatrix/util/OSUtils.java
+++ src/com/mitsuki/jmatrix/util/OSUtils.java
@@ -2,19 +2,55 @@
 /*       OSUtils       */
 // ------------------- //
 
+// -**- Package -**- //
 package com.mitsuki.jmatrix.util;
 
+// -**- Built-in Package -**- //
 import java.io.File;
 
+/**
+* This class provides operating system utilities<br>
+*
+* @version 1.0
+* @since   1.0.0
+* @author  Ryuu Mitsuki
+*
+* @see     com.mitsuki.jmatrix.util.Options
+* @see     com.mitsuki.jmatrix.util.XMLParser
+*/
 public class OSUtils
 {
+    /**
+    * List name of operating system.<br>
+    * Uses {@link OSUtils#getOSName} to gets current operating system.<br>
+    *
+    * @since 1.0.0
+    * @see   OSUtils#getOSName
+    */
     public enum OS {
         WINDOWS, LINUX, MAC, SOLARIS, OTHER
     };
 
-    public final static String sep = File.separator; // get path separator
     private static OS os = null;
 
+    /**
+    * Gets path separator determined by operating system.<br>
+    *
+    * @since 1.0.0
+    * @see   File#separator
+    */
+    public final static String sep = File.separator;
+
+    /**
+    * Method that gets current operating system name.<br>
+    * If operating system name not available on {@link #OS}, then it will returns {@code OTHER} value.<br>
+    *
+    * @return the name of current operating system
+    *
+    * @since  1.0.0
+    * @see    System#getProperty
+    * @see    OSUtils#OS
+    */
     public static OS getOSName() {
         if (os == null) {
             final String osName = System.getProperty("os.name").toLowerCase();
