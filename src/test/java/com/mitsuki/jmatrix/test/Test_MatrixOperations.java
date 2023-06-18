fd07af23c22f3e5a4ade7ff8dbe48dacd26002be
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-06-18 18:29:03 +0700

    Added `test_MatrixSubtraction`
    Method that checks and tests the subtraction results from both operands,
    and both operands also get some tests before operate subtraction (e.g. check whether both operands are same dimensions).

--- src/test/java/com/mitsuki/jmatrix/test/Test_MatrixOperations.java
+++ src/test/java/com/mitsuki/jmatrix/test/Test_MatrixOperations.java
@@ -35,4 +35,39 @@ public class Test_MatrixOperations {
         // Check the addition results
         assertEquals(true, Matrix.sum(m, n).equals(res));
     }
+
+    @Test
+    public void test_MatrixSubtraction() {
+        Matrix m = new Matrix(new double[][] {
+            { 5, -1 },
+            { 8, 16 },
+            { 4, 10 },
+            { 1, -5 }
+        });
+
+        Matrix n = new Matrix(new double[][] {
+            { 15, -18 },
+            { 6, 21 },
+            { 0, -3 },
+            { -1, 4 }
+        });
+
+        Matrix res = new Matrix(new double[][] {
+            { -10, 17 },
+            { 2, -5 },
+            { 4, 13 },
+            { 2, -9 }
+        });
+
+        assertEquals(false, MatrixUtils.isNullEntries(m));
+        assertEquals(false, MatrixUtils.isNullEntries(n));
+        assertEquals(false, m.equals(n));
+        assertEquals(false, m.isSquare());
+
+        // Before operate the subtraction, ensure both operands are same dimensions
+        assertEquals(true, MatrixUtils.isEqualsSize(m, n));
+
+        // Check the subtraction results
+        assertEquals(true, Matrix.sub(m, n).equals(res));
+    }
 }
