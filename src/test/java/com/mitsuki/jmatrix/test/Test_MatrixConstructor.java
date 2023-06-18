30a8e65622cb2630cd6bed659ae6cdd0b457f31e
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-06-18 13:08:26 +0700

    Added `Test_MatrixConstructor.java`

---
+++ src/test/java/com/mitsuki/jmatrix/test/Test_MatrixConstructor.java
@@ -0,0 +1,40 @@
+package com.mitsuki.jmatrix.test;
+
+import com.mitsuki.jmatrix.Matrix;
+import com.mitsuki.jmatrix.util.MatrixUtils;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Test_MatrixConstructor {
+    @Test
+    public void test_MatrixWithNullEntries() {
+        Matrix x = new Matrix();
+        Matrix y = new Matrix();
+
+        assertEquals(true, MatrixUtils.isNullEntries(x));
+        assertEquals(true, (x.getEntries() == null));
+        assertEquals(true, (0 == x.getSize()[0] && 0 == x.getSize()[1]));
+        assertEquals(true, x.equals(y));
+    }
+
+    @Test
+    public void test_NullMatrix() {
+        Matrix m = new Matrix(3, 1);
+        Matrix n = new Matrix(4, 5);
+
+        assertEquals(true,
+            MatrixUtils.isEquals(new double[][] {
+                {0},
+                {0},
+                {0}
+            }, m.getEntries())
+        );
+
+        assertEquals(false, MatrixUtils.isNullEntries(m));
+        assertEquals(true, (3 == m.getSize()[0] && 1 == m.getSize()[1]));
+        assertEquals(false, m.isSquare());
+        assertEquals(true, m.equals(m));
+        assertEquals(false, m.equals(n));
+    }
+}
