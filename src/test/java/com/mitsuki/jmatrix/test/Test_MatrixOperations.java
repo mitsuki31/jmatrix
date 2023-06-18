bfc4076e0a4eb06b403a390cb335b2465fa842c1
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-06-18 17:45:20 +0700

    Added `Test_MatrixOperations.java`
    A class that tests and checks the equality of results from all matrix operations that JMatrix currently has.

---
+++ src/test/java/com/mitsuki/jmatrix/test/Test_MatrixOperations.java
@@ -0,0 +1,35 @@
+package com.mitsuki.jmatrix.test;
+
+import com.mitsuki.jmatrix.Matrix;
+import com.mitsuki.jmatrix.util.MatrixUtils;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Test_MatrixOperations {
+    @Test
+    public void test_MatrixAddition() {
+        Matrix m = new Matrix(new double[][] {
+            { 1, 3, 5 },
+            { 5, 7, 4 }
+        });
+
+        Matrix n = new Matrix(new double[][] {
+            { 6, 7, -5 },
+            { -10, 5, 16 }
+        });
+
+        Matrix res = new Matrix(new double[][] {
+            { 7, 10, 0 },
+            { -5, 12, 20 }
+        });
+
+        assertEquals(false, MatrixUtils.isNullEntries(m));
+        assertEquals(false, MatrixUtils.isNullEntries(n));
+        assertEquals(false, m.equals(n));
+        assertEquals(false, m.isSquare());
+
+        // Check the addition operation
+        assertEquals(true, Matrix.sum(m, n).equals(res));
+    }
+}
