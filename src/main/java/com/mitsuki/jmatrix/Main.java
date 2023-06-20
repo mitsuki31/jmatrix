e075340dd330fde00f66a41b3f86963c6426c827
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-06-20 21:29:46 +0700

    Improve the JMatrix `Main` class (#49)

--- src/main/java/com/mitsuki/jmatrix/Main.java
+++ src/main/java/com/mitsuki/jmatrix/Main.java
@@ -3,25 +3,39 @@ package com.mitsuki.jmatrix;
 import com.mitsuki.jmatrix.util.Options;
 import com.mitsuki.jmatrix.util.XMLParser;
 
+/**
+ * Main class for <b>JMatrix</b> library that provides some information such as <b>JMatrix</b> version.
+ *
+ * <p>This class does not provides API for build the matrix,
+ * it's just <i>useless<i> class if get imported.
+ *
+ * <p><b>Usage:</b></p>
+ *
+ * <pre>&nbsp;
+ *   java -jar path/to/jmatrix.jar [-h|-V|-cr]
+ * </pre>
+ *
+ * @author   <a href="https://github.com/mitsuki31" target="_blank">
+ *           Ryuu Mitsuki</a>
+ * @version  1.2, 20 June 2023
+ * @since    1.0.0
+ * @see      com.mitsuki.jmatrix.Matrix
+ */
 public class Main
 {
+    private static XMLParser XML = new XMLParser(XMLParser.XMLType.CONFIG);
+    private static String programVersion = XML.getProperty("programName") + " " + getVersion();
+
     public static void main(String[ ] args) {
         String arg1 = null;
-        String arg2 = null;
-
-        XMLParser XML = new XMLParser(XMLParser.XMLType.CONFIG);
 
         if (args.length >= 1 && args.length < 2) {
             arg1 = args[0];
-            if (args.length == 2) {
-                arg2 = args[1];
-            }
-        }
-        else if (args.length > 1) {
+        } else if (args.length > 1) {
             try {
                 throw new IllegalArgumentException("Too much arguments");
-            } catch (final Exception e) {
-                Options.raiseErrorMsg(e, 0);
+            } catch (final IllegalArgumentException iae) {
+                Options.raiseErrorMsg(iae, 0);
                 System.out.println(Options.getHelpMsg()[0]);
                 System.out.println("    " +
                     "java -jar <jar_file> [-h|-V|-cr]");
@@ -46,25 +60,16 @@ public class Main
 
         switch (Options.getOptions(arg1)) {
             case VERSION:
-                System.out.print(XML.getProperty("programName") + " " +
-                    XML.getProperty("version") + "-");
-                System.out.print(XML.getProperty("releaseType"));
+                String pkgName = String.format(" <%s>", XML.getProperty("packageName"));
 
-                if (!XML.getProperty("releaseType").equals("release"))
-                    System.out.print("." + XML.getProperty("betaNum"));
-
-                System.out.printf(" <%s>%s", XML.getProperty("packageName"), System.lineSeparator());
+                System.out.println(programVersion + pkgName);
                 break;
 
             case HELP:
-                System.out.print(XML.getProperty("programName") + " " + XML.getProperty("version"));
+                String lines = System.lineSeparator();
+                for (byte i = 0; i < (byte) programVersion.length(); i++) lines += "-";
 
-                if (!XML.getProperty("releaseType").equals("release")) {
-                    System.out.println("-" + XML.getProperty("releaseType") + "." + XML.getProperty("betaNum"));
-                } else {
-                    System.out.print(System.lineSeparator());
-                }
-
+                System.out.println(programVersion + lines + System.lineSeparator());
                 for (String s : Options.getHelpMsg()) System.out.println(s);
                 break;
 
@@ -72,5 +77,22 @@ public class Main
                 for (String s : Options.getCopyright()) System.out.println(s);
                 break;
         }
+    }
+
+    private static String getVersion() {
+        String ver = XML.getProperty("version");
+
+        // Only retrieve the release type if not release and stable release
+        if (!XML.getProperty("releaseType").equals("release") ||
+                !XML.getProperty("releaseType").equals("stable")) {
+            ver += "-" + XML.getProperty("releaseType");
+
+            // This will only retrieve a non-zero beta number
+            if (XML.getProperty("betaNum").equals("0")) {
+                ver += "." + XML.getProperty("betaNum");
+            }
+        }
+
+        return ver;
     }
 }
