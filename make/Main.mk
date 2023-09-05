05817a3442a1cef9518b5a9c664d7e94297c817d
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-09-05 17:12:29 +0700

    Several addition and enhancements
    * Defined several new functions (exclusive to `Main.mk`)
        - `__pr_lint`
        - `__pr_verbose`
        - `__pr_include_src`
    
    * Implemented several target rules for build and clean:
        - `compile`
        - `package`
        - `clean`
        - `cleanbin`
    
    * Code enhancements and improvements

--- make/Main.mk
+++ make/Main.mk
@@ -17,104 +17,168 @@
 # limitations under the License.
 #
 
-include $(shell pwd)/make/Setup.mk
 
-
-# Enable the linter if '.lint' is present in command line arguments or
-# LINT variable are defined and the value is 'true'.
-#
-# For example:
-#   $ make <TARGET> .lint
-#
-# It is equivalent with:
-#   $ make <TARGET> LINT=true
-#
-ifeq ($(filter $(MAKECMDGOALS),.lint),.lint)
-  MAKECMDGOALS  := $(strip $(subst .lint,,$(MAKECMDGOALS)))
-
-  JFLAGS        += -Xlint:all -Xdoclint:all
-  __intern_LINT := 1
-  LINT          := true
-else
-  # Enable the linter with all checks if LINT is true
-  ifeq ($(LINT),true)
-    JFLAGS        += -Xlint:all -Xdoclint:all
-    __intern_LINT := 1
-  else
-    JFLAGS        += -Xlint:deprecation,unchecked,cast -Xdoclint:html,syntax/protected
-  endif
-endif
-
-
-# Enable the verbose output if '.verbose' is present in command line arguments or
-# VERBOSE variable are defined and the value is 'true'.
-#
-# For example:
-#   $ make <TARGET> .verbose
-#
-# It is equivalent with:
-#   $ make <TARGET> VERBOSE=true
-#
-ifeq ($(filter $(MAKECMDGOALS),.verbose),.verbose)
-  MAKECMDGOALS     := $(strip $(subst .verbose,,$(MAKECMDGOALS)))
-
-  JFLAGS           += -verbose
-  VERBOSE          := true
-  __intern_VERBOSE := 1
-else
-  # Enable the verbose if VERBOSE is true
-  ifeq ($(VERBOSE),true)
-    JFLAGS           += -verbose
-    __intern_VERBOSE := 1
-  endif
-endif
-
-
-# Enable option to include the source files get archived if '.include-src' is present
-# in command line arguments or INCLUDE_SRC variable are defined and
-# the value is 'true'.
-#
-ifeq ($(filter $(MAKECMDGOALS),.include-src),.include-src)
-  MAKECMDGOALS       := $(strip $(subst .include-src,,$(MAKECMDGOALS)))
-
-  __intern_INC_SRC   := 1
-else
-  ifeq ($(INCLUDE_SRC),true)
-    __intern_INC_SRC := 1
-  endif
-endif
+# Import: Setup.mk
+include $(or $(MAKE_DIR),$(shell pwd)/make)/Setup.mk
 
 
 ## :::::::::::::::: ##
-##  Target Rules    ##
+##  Functions       ##
 ## :::::::::::::::: ##
+#
+# Note:
+# These functions are exclusive for this file only
 
 
-# Default target rule; If no target rule specified then display the help message
-help:
-	@cat $(word 1,$(MAKE_USAGE))
-
-.PHONY: help
-
-
-# We cannot do more further here, when the one of Java source file get changed
-# or modified, then it will compile all source files again, even though it is only one of them.
-# If you want more efficient and more easier on compilation, consider to use Maven instead.
-$(CLASSES_FILES): $(SOURCES)
-	$(eval JFLAGS += $(FLAGS) $(addprefix -J,$(MEMFLAGS)))
-
+# __pr_lint Function
+#
+# This function will displays the condition of LINT option to console.
+#
+# Usage:
+#   $(call __pr_lint)
+#
+define __pr_lint
 	$(info $(CLR_PREFIX) $(call __clr_br,5,LINT):$(if $(__intern_LINT),\
 		$(call __clr_br,2,ENABLED),\
 		$(call __clr_br,1,DISABLED)\
 	))
+endef
 
-	$(info $(CLR_PREFIX) Compiling all Java source files...)
-	$(foreach src,$^,\
-		$(info $(call __clr_br,3,   *) $(call __bold,$(notdir $(src))):\
-		$(subst /,.,$(basename $(subst $(JAVA_DIR)/,,$(call __clr,6,$(src))))))\
+
+# __pr_verbose Function
+#
+# This function will displays the condition of VERBOSE option to console.
+#
+# Usage:
+#   $(call __pr_verbose)
+#
+define __pr_verbose
+	$(info $(CLR_PREFIX) $(call __clr_br,5,VERBOSE):$(if $(__intern_VERBOSE),\
+		$(call __clr_br,2,ENABLED),\
+		$(call __clr_br,1,DISABLED)\
+	))
+endef
+
+
+# __pr_include_src Function
+#
+# This function will displays the condition of INCLUDE_SRC option to console.
+#
+# Usage:
+#   $(call __pr_include_src)
+#
+define __pr_include_src
+	$(info $(CLR_PREFIX) $(call __clr_br,5,INCLUDE_SRC):$(if $(__intern_INC_SRC),\
+		$(call __clr_br,2,ENABLED),\
+		$(call __clr_br,1,DISABLED)\
+	))
+endef
+
+
+
+## :::::::::::::::: ##
+##  Builders        ##
+## :::::::::::::::: ##
+
+
+compile: $(CLASSES_FILES)
+	@:
+
+
+package: compile
+	$(call __pr_include_src)
+	$(call __pr_verbose)
+	$(call __info,)
+
+	$(eval JARFLAGS := $(subst {MANIFEST},$(MANIFEST),$(JARFLAGS)) $(FLAGS))
+
+	$(call __info,Copying the resources...)
+	$(call __info,Got $(words $(shell find $(RESOURCES) -type f)) resources)
+	@cp --recursive --preserve=all $(RESOURCES) $(CLASSES_DIR) \
+		$(if $(__intern_VERBOSE),--verbose,)
+
+	$(call __info,)
+	$(call __info,Creating the JAR file...)
+	@$(JAR) $(subst {JAR},$(word 1,$(JAR_NAMES)),$(JARFLAGS)) $(MANIFEST)     \
+		$(wildcard LICENSE) -C $(CLASSES_DIR) .
+
+# Create the second JAR file containing the source files only,
+# if and only if the INCLUDE_SRC option is "true" (enabled)
+ifeq ($(__intern_INC_SRC),1)
+	$(call __info,)
+	$(call __info,Creating the JAR file (sources)...)
+	@$(JAR) $(subst {JAR},$(word 2,$(JAR_NAMES)),$(JARFLAGS)) $(MANIFEST) \
+		$(wildcard LICENSE) -C $(JAVA_DIR) . $(addsuffix \ .,$(addprefix -C ,$(foreach f,$(RESOURCES),\
+		$(addsuffix $(notdir $(f)),$(CLASSES_DIR)/))))
+endif  # __intern_INC_SRC
+
+
+build-docs:
+# TODO: Implement the code for build-docs target
+	@:
+
+.PHONY: compile package build-docs
+
+
+
+## :::::::::::::::: ##
+##  Cleaners        ##
+## :::::::::::::::: ##
+
+
+clean:
+	$(call __pr_verbose)
+	$(call __info,)
+
+# Clean the "target" (which is the output directory for compiled classes
+# and the JAR files) recursively.
+ifeq ($(call __is_exist,$(TARGET_DIR)),1)
+	$(call __info,Cleaning up \"$(TARGET_DIR)\" directory recursively...)
+	@-rm --recursive $(TARGET_DIR) $(if $(__intern_VERBOSE),--verbose,)
+else
+	$(call __info,Cleaned up \"$(TARGET_DIR)\" directory)
+endif
+
+	@echo "$(CLR_PREFIX) $(call __clr_br,2,All clean)"
+
+
+cleanbin:
+	$(call __pr_verbose)
+	$(call __info,)
+
+ifeq ($(call __is_exist,$(CLASSES_DIR)),1)
+	$(call __info,Cleaning up \"$(CLASSES_DIR)\" directory recursively...)
+	@-rm -r $(CLASSES_DIR) $(if $(__intern_VERBOSE),--verbose,)
+else
+	$(call __info,Cleaned up \"$(CLASSES_DIR)\" directory)
+endif
+
+	@echo "$(CLR_PREFIX) $(call __clr_br,2,All clean)"
+
+
+cleandocs:
+# TODO: ...
+	@:
+
+.PHONY: clean cleanbin cleandocs
+
+
+# We cannot do any further from here, when the one of Java source file get changed
+# or modified, then it will compile all source files again, even though it is only one of them.
+# If you want more efficient and more easier on compilation, consider to use Maven instead.
+#
+$(CLASSES_FILES): $(SOURCES)
+	$(call __pr_lint)
+	$(call __pr_verbose)
+	$(call __info,)
+
+	$(call __info,Compiling all Java source files...)
+	@$(foreach src,$^,\
+		echo "$(CLR_PREFIX) $(call __bold,$(notdir $(src))) >>\
+		$(subst /,.,$(basename $(subst $(JAVA_DIR)/,,$(call __clr,4,$(src)))))$(NORMAL)";\
 	)
 
-	@$(JC) -d $(CLASSES_DIR) $^ $(JFLAGS)
+	@$(JC) -d $(CLASSES_DIR) $^ $(JCFLAGS) $(addprefix -J,$(MEMFLAGS))
 
 
 
