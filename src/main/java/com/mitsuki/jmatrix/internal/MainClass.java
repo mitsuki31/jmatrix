bcf1f172924d821eb3b34174d43f748089cffce8
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-09-16 20:09:41 +0700

    Refactor the main class
    * Changed the main class to `MainClass.java`
    * Introduced a new final class called `ArgumentsParser` in main class
    * Added several new methods to `ArgumentsParser` class, including:
      - `contains`
      - `get`
      - `getArguments`
      - `iterator` (from `Iterable` interface)

---
+++ src/main/java/com/mitsuki/jmatrix/internal/MainClass.java
@@ -0,0 +1,155 @@
+// ------------------- //
+/*      MainClass      */
+// ------------------- //
+
+/* Copyright (c) 2023 Ryuu Mitsuki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mitsuki.jmatrix.internal;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.lang.Iterable;
+
+
+/**
+ * Main class for <b>JMatrix</b> library that provides some information
+ * such as <b>JMatrix</b> version. If this class is imported, it will be
+ * not valuable because it does not provides APIs to construct the matrices.
+ *
+ * As of JMatrix 1.5, the class name has changed from
+ * {@code Main} to {@code MainClass}.
+ *
+ * @author   <a href="https://github.com/mitsuki31">Ryuu Mitsuki</a>
+ * @version  1.4, 16 September 2023
+ * @since    1.0.0b.1
+ * @see      com.mitsuki.jmatrix.Matrix
+ */
+public class MainClass {
+
+    /**
+     * Main method for <b>JMatrix</b> library.
+     *
+     * @param args  a {@code String} array containing arguments from command line.
+     *
+     * @since       1.0.0b.1
+     */
+    public static void main(String[] args) {
+        ArgumentsParser<String> argsParser = new ArgumentsParser<>(args);
+        List<String> argsList = argsParser.getArguments();
+
+        argsList.forEach((arg) -> {
+            System.out.print(arg + argsList.indexOf(arg) ", ");
+        });
+        System.out.print(System.lineSeparator());
+    }
+}
+
+
+/**
+ * An utility class for parsing and managing a set of unique arguments.
+ * It is designed only for {@link MainClass} class to parse the
+ * command line arguments.
+ *
+ * @param   <E>  The type of object stored in this class.
+ *
+ * @author       <a href="https://github.com/mitsuki31">Ryuu Mitsuki</a>
+ * @version      1.0, 16 September 2023
+ * @since        1.5.0
+ */
+final class ArgumentsParser<E> implements Iterable<E> {
+    private List<E> arguments;
+
+    /**
+     * Sole constructor. Constructs this class with the specified array of arguments.
+     * Duplicate elements are removed, and the order is preserved.
+     *
+     * @param args  The array of arguments to parse.
+     *
+     * @since       1.5.0
+     */
+    ArgumentsParser(E[] args) {
+        // Using LinkedHashSet to remove duplicate elements and
+        // preserve the order of elements
+        Set<E> argsSet = new LinkedHashSet<E>(Arrays.asList(args));
+
+        // Convert to List after removing all duplicate elements
+        this.arguments = new ArrayList<E>(argsSet);
+    }
+
+
+    /**
+     * Checks whether this parser contains the specified element.
+     *
+     * @param  o  element whose presence in this list is to be tested.
+     *
+     * @return    {@code true} if the element is present, {@code false} otherwise.
+     *
+     * @since     1.5.0
+     */
+    boolean contains(Object o) {
+        return this.arguments.contains(o);
+    }
+
+    /**
+     * Returns the element at the specified position in this list of arguments.
+     *
+     * @param  index  The index of the element to retrieve
+     *                (index > 0 && index < size()).
+     *
+     * @return        The element at the specified index in this list.
+     *
+     * @since         1.5.0
+     *
+     * @throws IndexOutOfBoundsException
+     *         if the index is out of range (index < 0 || index > size()).
+     */
+    E get(int index) {
+        return this.arguments.get(index);
+    }
+
+    /**
+     * Returns an unmodifiable view of the arguments contained in this class.
+     * Modifications to the returned list result in an
+     * {@code UnsupportedOperationException}.
+     *
+     * @return An unmodifiable view of the arguments.
+     *
+     * @since  1.5.0
+     */
+    List<E> getArguments() {
+        // Return the properties with read-only attribute,
+        // this means users cannot modify the elements.
+        return Collections.unmodifiableList(this.arguments);
+    }
+
+    /**
+     * Returns an iterator over the elements in this list of arguments
+     * in proper sequence.
+     *
+     * @return An iterator over the elements.
+     *
+     * @since  1.5.0
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return this.arguments.iterator();
+    }
+}
