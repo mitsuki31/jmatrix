f4e0f6c057c35beb64284ee8b35d4b2aa4e7a13a
Ryuu Mitsuki <dhefam31@gmail.com>
2024-09-16 18:54:01 +0700

    Deploy API site v1.5.0-SNAPSHOT
    Signed-off-by: Ryuu Mitsuki <dhefam31@gmail.com>

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.mitsuki.jmatrix"},{"l":"com.mitsuki.jmatrix.core"},{"l":"com.mitsuki.jmatrix.enums"},{"l":"com.mitsuki.jmatrix.exception"}]
